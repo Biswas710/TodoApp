9e7a01bef8ac10c55f02b2c1d9fd72d9c0e52da1
Biswas710 <biswasthapa1035@gmail.com>
2025-03-11 20:13:02 +0530

    Integrated bootstrap for better ui and also implemented some endpoints

--- todoapp/src/main/java/com/app/todoapp/Controller/TaskController.java
+++ todoapp/src/main/java/com/app/todoapp/Controller/TaskController.java
@@ -5,11 +5,8 @@ import com.app.todoapp.Service.TaskService;
 import com.app.todoapp.models.Task;
 import com.sun.source.util.TaskListener;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -34,4 +31,15 @@ public class TaskController {
         taskService.createTask(title);
         return "redirect:/";
     }
+    @GetMapping("{id}/delete")
+    public String deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+    @GetMapping("{id}/toggle")
+    public String toggleTask(@PathVariable Long id) {
+        taskService.toggleTask(id);
+        return "redirect:/";
+    }
+
 }
